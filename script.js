6409d5faf56e0ca2e3308ce1c2550243a4aea602
niu tech <jerzyglowacki@gmail.com>
2017-08-08 12:31:56 +0200

    HTTPS support

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function loadURL(url, no) {
 	var iframe = document.getElementsByTagName('iframe')[no];
 	iframe.url = url;
 	var script = document.createElement('script');
-	script.src = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20data.headers%20where%20url%3D%22' + encodeURIComponent(url) + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=getData' + no;
+	script.src = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20data.headers%20where%20url%3D%22' + encodeURIComponent(url) + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=getData' + no;
 	document.body.appendChild(script);
 	setTimeout(function () {
 		var spinner = '<style>@keyframes rotate { from { transform: rotate(0deg); } to { transform: rotate(360deg); } }</style><div style="border: 6px solid #eee; border-top-color: #999; border-radius: 50%; position: absolute; top: 50%; left: 50%; margin: -20px 0 0 -20px; height: 40px; width: 40px; animation: rotate 1s infinite linear;"></div>';
